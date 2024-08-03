dc4bb102f0a655dce1b305fb15a973ceff39ee34
bingqiu456 <bingyuevip@gmail.com>
2024-08-04 00:47:59 +0800

    修复了匹配算法的致命bug\n增加变量的适配\n优化代码结构

--- src/plugins/dic_match.py
+++ src/plugins/dic_match.py
@@ -9,6 +9,7 @@ https://github.com/bingqiu456
 from . import start_dic
 from . import load_file
 from . import variable_main
+from . import variable_v
 
 async def match_sentence(event: any, word: str) -> str:
     
@@ -24,13 +25,13 @@ async def match_sentence(event: any, word: str) -> str:
 
     while p_start <= p_end:
 
-        now_text = dic_json[p_start][:-1].replace("'\n'","\n").replace("'\r'","\r") # 先转移一下换行
+        now_text = dic_json[p_start][:-1]
 
         ans+= await get_var(event, now_text)
         p_start+=1
     
-    print(ans)
-    return ans
+   
+    return ans.replace("\\n","\n").replace("\\r","\r") # 换行转译
 
 async def get_var(event: any, s: str) -> str:
     """
@@ -40,28 +41,53 @@ async def get_var(event: any, s: str) -> str:
     ans = ""
     k = []
     temp = ""
+    m = {
+        "$":"variable_u_event",
+        "%":"variable_main_event"
+    }
     for i in range(len(s)):
         if s[i] in ["$","%"]:
             if s[i] in k and k.pop() == s[i]:
-                ans+= await variable_main_event(event,temp+s[i])
+                try:
+                    ans+= await variable.__dict__.get(m[s[i]]).__func__(event,temp+s[i])
+                except:
+                    pass
                 temp = ""
+                continue
             else:
                 k.append(s[i])
-                temp += s[i]
-        else:
-            temp += s[i]
+        
+        temp += s[i]
+
+    if k:
+        if k.pop() == temp[0]:
+            ans+= await variable.__dict__.get(m[temp[0]]).__func__(event,temp+temp[0])
+            temp = ""
+  
     ans+=temp
     return ans
 
+class variable:
 
-async def variable_main_event(event: any, s: str) -> str:
-    "变量对应名字的扩展"
-    v_ = {
-        "%QQ%":"QQ",
-        "%Robot%":"bot_qq",
-        "%消息id%":"message_id",
-        "%群号%":"group_id"
-    }
-    p = variable_main.Variable
-    k = p.__dict__.get(v_[s]).__func__(event)
-    return k
+    @staticmethod
+    async def variable_main_event(event: any, s: str) -> str:
+        "%变量对应名字的扩展"
+        v_ = {
+            "%QQ%":"QQ",
+            "%Robot%":"bot_qq",
+            "%消息id%":"message_id",
+            "%群号%":"group_id"
+        }
+        p = variable_main.Variable
+        k = p.__dict__.get(v_[s]).__func__(event)
+        return k
+
+    @staticmethod
+    async def variable_u_event(event: any, s: str) -> str:
+        "$变量对应名字的扩展"
+        v_ = {
+            "$管理员$"
+        }
+        p = variable_v.variable
+        k = p.__dict__.get(v_[s]).__func__(event)
+        return k
---
+++ src/plugins/variable_v.py
@@ -0,0 +1,11 @@
+"""
+本模块是bingyue_dic的变量第二分装层
+https://github.com/bingqiu456
+点个星星 行不行
+"""
+
+class variable:
+
+    @staticmethod
+    def admin(event, *args):
+        print(args)
